e069f06a3cf950b7269776feb950139f3062a249
Lic. Juan Diego Hernandez <hernandezrjd26@gmail.com>
2022-08-19 10:21:07 -0500

    CREACION DE PROYECTO

---
+++ src/org/dao/register/DaoRegister.java
@@ -0,0 +1,23 @@
+package org.dao.register;
+
+import java.util.List;
+
+import org.modelo.register.Register;
+
+/*
+ * 
+ * INTERFACE: NO SE PROGRAMA, SOLO SE AGREGAN LOS METODOS QUE SE VA A IMPLEMENTAR
+ * 
+ * 
+ * 
+ */
+
+public interface DaoRegister {
+	
+	public void addRegister(Register register);
+	public List<Register> listRegisters();
+	public Register getRegisterById(int id);
+	public void deleteRegister(int id);
+	
+
+}
